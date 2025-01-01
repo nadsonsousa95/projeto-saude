b11ef301c912e24701ceb6bd8acf4ad60babfa46
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-01-01 18:43:00 -0300

    Ajustes de design

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-// Seleciona o botão e o menu
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+    // Função para calcular o IMC
+    function calcularIMC() {
+        var peso = document.getElementById('peso').value;
+        var altura = document.getElementById('altura').value;
 
-// Adiciona o evento de clique ao botão
-menuToggle.addEventListener('click', () => {
-    // Alterna entre abrir e fechar o menu
-    menu.classList.toggle('open');
-});
-
-// Fecha o menu automaticamente ao clicar em qualquer link (em dispositivos móveis)
-const menuLinks = document.querySelectorAll('.menu li a');
-menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        // Fecha o menu ao clicar em um link, em dispositivos móveis
-        if (menu.classList.contains('open')) {
-            menu.classList.remove('open');
+        // Verificar se os campos estão preenchidos
+        if (peso === '' || altura === '') {
+            alert('Por favor, preencha todos os campos.');
+            return;
         }
-    });
-});
+
+        // Calcular o IMC
+        var imc = peso / (altura * altura);
+        imc = imc.toFixed(2); // Arredonda o IMC para 2 casas decimais
+
+        // Classificação do IMC
+        var classificacao = '';
+
+        if (imc < 18.5) {
+            classificacao = 'Abaixo do peso';
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            classificacao = 'Peso normal';
+        } else if (imc >= 25 && imc <= 29.9) {
+            classificacao = 'Sobrepeso';
+        } else if (imc >= 30 && imc <= 34.9) {
+            classificacao = 'Obesidade Grau I';
+        } else if (imc >= 35 && imc <= 39.9) {
+            classificacao = 'Obesidade Grau II';
+        } else {
+            classificacao = 'Obesidade Grau III';
+        }
+
+        // Exibir o resultado
+        var resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.innerHTML = 'Seu IMC é: <strong>' + imc + '</strong><br>Classificação: ' + classificacao;
+    }
